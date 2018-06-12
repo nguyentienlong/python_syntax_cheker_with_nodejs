008fcc4e40ae8375ae6d768b061601ab16654aed
longka <long@ahamove.com>
2018-06-12 12:00:57 +0700

    change var

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const server = http.createServer();
 server.on('request', (req, res) => {
   if (req.url === '/validate-python') {
     // todo parse formular1 fro request
-    const validFormular = "(20000 if x <= 4 else 20000 + (x - 4) *5000 ";
+    const formular = "(20000 if x <= 4 else 20000 + (x - 4) *5000 ";
 
     // formular2 = '(20000 if x <= 4 else 20000 + (x - 4) *5000)';
     const tempPyFile = '/tmp/temp.py';
 
-    fs.writeFile('/tmp/temp.py', validFormular, (err) => {
+    fs.writeFile('/tmp/temp.py', formular, (err) => {
         // throws an error, you could also catch it here
         if (err) throw err;
         // success case, the file was saved
